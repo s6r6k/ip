facb1782ff84ccd537c0d13a58b97d04fdeebede
Sara <s6r6kh6n@gmail.com>
2026-01-21 22:14:42 +0800

    Make Duke echo user commands and exit on 'bye'

--- src/main/java/Fuzzy.java
+++ src/main/java/Fuzzy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Fuzzy {
     public static void main(String[] args) {
         String line = "";
@@ -7,7 +9,15 @@ public class Fuzzy {
         System.out.println(line);
         String greeting = "Hello I'm Fuzzy!" + "\n" + "What can I do for you babe?";
         System.out.println(greeting + "\n" + line);
-        System.out.println("Bye. See you soon.");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            System.out.println(line);
+            input = scanner.nextLine();
+        }
+        System.out.println("Alright, see you again.");
+        scanner.close();
         System.out.println(line);
     }
 }
