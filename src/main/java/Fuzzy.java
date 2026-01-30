2b971e62f8e7d6b36d64825c987bddb24478c37b
Sara <s6r6kh6n@gmail.com>
2026-01-30 15:18:05 +0800

    Implement Level 7 save and load functions

--- src/main/java/Fuzzy.java
+++ src/main/java/Fuzzy.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 class Task {
     private boolean completed;
     private String name;
@@ -73,10 +77,78 @@ class EmptyDescException extends Exception {
     }
 }
 public class Fuzzy {
+    public static void saveTasks(ArrayList<Task> list) {
+        try {
+            File folder = new File("./data");
+            if (!folder.exists()) folder.mkdir();
+            File file = new File("./data/fuzzy.txt");
+            FileWriter fw = new FileWriter("./data/fuzzy.txt");
+            for(Task taski : list) {
+                String type = "";
+                String extra = "";
+                if(taski instanceof ToDo) type = "T";
+                else if(taski instanceof Deadline) {
+                    type = "D";
+                    extra = ((Deadline) taski).deadline;
+                }
+                else if(taski instanceof Event) {
+                    type = "E";
+                    extra = ((Event) taski).startTime + "-" + ((Event) taski).endTime;
+                }
+                int complete = taski.isCompleted()? 1:0;
+                if(extra.equals("")) fw.write(type + " | " + complete + " | " + taski.getName() + "\n");
+                else {
+                    fw.write(type + " | " + complete + " | " + taski.getName() + " | " + extra + "\n");
+                }
+            }
+            fw.close();
+        } catch(IOException e) {
+            System.out.println("Yaar there was an error in saving: " + e.getMessage());
+        }
+    }
 
-    public static void main(String[] args) {
+    public static ArrayList<Task> taskLoader() {
         ArrayList<Task> list = new ArrayList<>();
-        int totalTasks = 0;
+        File file = new File("./data/fuzzy.txt");
+        if (!file.exists()) return list;
+        try {
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] parts = line.split(" \\| ");
+                if(parts.length < 3) continue;
+                String type = parts[0];
+                boolean done = parts[1].equals("1");
+                String name = parts[2];
+                Task task = null;
+
+                switch (type) {
+                    case "T":
+                        task = new ToDo(name);
+                        break;
+                    case "D":
+                        if (parts.length < 4) continue;
+                        task = new Deadline(parts[3], name);
+                        break;
+                    case "E":
+                        if (parts.length < 4) continue;
+                        String[] times = parts[3].split("-");
+                        if (times.length < 2) continue;
+                        task = new Event(times[0], times[1], name);
+                        break;
+                }
+
+                if (task != null && done) task.complete();
+                if (task != null) list.add(task);
+            }
+            sc.close();
+            } catch(IOException e) {
+            System.out.println("Error loading tasks: " + e.getMessage());
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        ArrayList<Task> list = taskLoader();
 
         String line = "";
         for (int i = 0; i < 35; i++) {
@@ -106,49 +178,51 @@ public class Fuzzy {
                     int taskNum = Integer.parseInt(input.split(" ", 2)[1]);
                     Task curry = list.get(taskNum - 1);
                     curry.complete();
+                    saveTasks(list);
                     System.out.println("I marked it, madam:" + "\n" + curry.toString());
                 } else if (command.equals("unmark")) {
                     int taskNum = Integer.parseInt(input.split(" ", 2)[1]);
                     Task curry = list.get(taskNum - 1);
                     curry.unmark();
+                    saveTasks(list);
                     System.out.println("Ok, unmarked it bubs:" + "\n" + curry.toString());
                 } else if (command.equals("todo")) {
-                    totalTasks++;
                     ToDo td = new ToDo(details);
                     list.add(td);
+                    saveTasks(list);
                     System.out.println(line);
-                    System.out.println("Alrighty, added it:" + "\n" + totalTasks + "." + td.toString() + "\n"
-                            + "You have " + totalTasks + " tasks darling");
+                    System.out.println("Alrighty, added it:" + "\n" + list.size() + "." + td.toString() + "\n"
+                            + "You have " + list.size() + " tasks darling");
                     System.out.println(line);
                 } else if (command.equals("deadline")) {
-                    totalTasks++;
                     String by = details.split("/by", 2)[1].trim();
                     String deets = details.split("/by", 2)[0].trim();
                     Deadline dl = new Deadline(by, deets);
                     list.add(dl);
+                    saveTasks(list);
                     System.out.println(line);
-                    System.out.println("Alrighty, added it:" + "\n" + totalTasks + "." + dl.toString() + "\n"
-                            + "You have " + totalTasks + " tasks darling");
+                    System.out.println("Alrighty, added it:" + "\n" + list.size() + "." + dl.toString() + "\n"
+                            + "You have " + list.size() + " tasks darling");
                     System.out.println(line);
 
                 } else if (command.equals("event")) {
-                    totalTasks++;
                     String afterFrom = details.split("/from", 2)[1];
-                    String beforeFrom = details.split("/from", 2)[0];
+                    String beforeFrom = details.split("/from", 2)[0].trim();
                     String start = afterFrom.split("/to", 2)[0];
                     String to = afterFrom.split("/to", 2)[1];
                     Event ev = new Event(start, to, beforeFrom);
                     list.add(ev);
+                    saveTasks(list);
                     System.out.println(line);
-                    System.out.println("Alrighty, added it:" + "\n" + totalTasks + "." + ev.toString() + "\n"
-                            + "You have " + totalTasks + " tasks darling");
+                    System.out.println("Alrighty, added it:" + "\n" + list.size() + "." + ev.toString() + "\n"
+                            + "You have " + list.size() + " tasks darling");
                     System.out.println(line);
                 } else if(command.equals("remove")) {
-                    totalTasks--;
                     int numOfTask = Integer.parseInt(details.trim());
                     Task taski = list.get(numOfTask - 1);
                     list.remove(numOfTask - 1);
-                    System.out.println("Okay bubs I removed: " + taski.toString() + "Now you have " + totalTasks + " tasks" +
+                    saveTasks(list);
+                    System.out.println("Okay bubs I removed: " + taski.toString() + "Now you have " + list.size() + " tasks" +
                             " tasks in the list");
 
                 } else {
