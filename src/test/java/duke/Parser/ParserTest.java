ddbf4a84d84a06e793fc124d92fa6e5a1f5865b6
Sara <s6r6kh6n@gmail.com>
2026-02-18 23:00:22 +0800

    Improve Parser tests and add cases for unknown and empty input

--- src/test/java/duke/Parser/ParserTest.java
+++ src/test/java/duke/Parser/ParserTest.java
@@ -1,42 +1,53 @@
 package duke.Parser;
+
 import duke.Exception.EmptyDescException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
+
     @Test
-    public void parserDeetsTest() throws EmptyDescException {
+    public void parse_validTodoWithSpaces_success() throws EmptyDescException {
         ParsedInput result = Parser.parse("  todo read book");
-        assertEquals("todo", result.getCommand());
+        assertEquals(Parser.CommandType.TODO, result.getCommandType());
         assertEquals("read book", result.getDetails());
     }
 
     @Test
-    public void parseList() throws EmptyDescException {
+    public void parse_listCommand_success() throws EmptyDescException {
         ParsedInput result = Parser.parse("list");
-        assertEquals("list", result.getCommand());
+        assertEquals(Parser.CommandType.LIST, result.getCommandType());
         assertEquals("", result.getDetails());
     }
 
     @Test
-    public void parseException() {
+    public void parse_missingDetails_throwsException() {
         assertThrows(EmptyDescException.class, () -> Parser.parse("todo"));
     }
 
     @Test
-    public void parseBye() throws EmptyDescException {
+    public void parse_byeCommand_success() throws EmptyDescException {
         ParsedInput result = Parser.parse("bye");
-        assertEquals("bye", result.getCommand());
+        assertEquals(Parser.CommandType.BYE, result.getCommandType());
         assertEquals("", result.getDetails());
     }
 
     @Test
-    public void parseDeadline() throws EmptyDescException {
+    public void parse_deadlineCommand_success() throws EmptyDescException {
         ParsedInput result = Parser.parse("deadline submit report /by 2005-01-16");
-        assertEquals("deadline", result.getCommand());
+        assertEquals(Parser.CommandType.DEADLINE, result.getCommandType());
         assertEquals("submit report /by 2005-01-16", result.getDetails());
     }
-}
 
-//valid input, parsers properly
-//invalid input, must handle correctly
+    @Test
+    public void parse_unknownCommand_returnsUnknown() throws EmptyDescException {
+        ParsedInput result = Parser.parse("blahblah something random");
+        assertEquals(Parser.CommandType.UNKNOWN, result.getCommandType());
+        assertEquals("blahblah something random", result.getDetails());
+    }
+
+    @Test
+    public void parse_emptyInput_throwsException() {
+        assertThrows(EmptyDescException.class, () -> Parser.parse(""));
+    }
+}
--- src/test/java/duke/ToDoTest.java
+++ src/test/java/duke/ToDoTest.java
@@ -1,10 +1,8 @@
 package duke;
 import duke.Task.ToDo;
-import org.junit.jupiter.api.Test; //Marks a method as a test case
-import static org.junit.jupiter.api.Assertions.*; //assertEquals etc
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-//structure of every JUnit test:
-//AAA: Arrange, set up objects, Act, call the method, Assert-check the result
 public class ToDoTest {
     @Test
     public void markAsDone() {
@@ -19,9 +17,3 @@ public class ToDoTest {
         assertFalse(todo.isCompleted());
     }
 }
-
-//JUNIT NOTES:
-//JUNIT LOOKS FOR CLASSES THAT END WITH TEST AND CHEKC THE METHODS FOR @Test
-//JUNIT will control when this method runs etc
-//so now we are checking if isCompleted is really true like it should be, test passed.
-//for 2nd method tdo object should not have been completed so its false.
